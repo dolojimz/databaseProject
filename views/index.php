05f350a120845710d22ea0d1b31bdb97861a98c5
dolojimz <jimmy.flores999@gmail.com>
2018-11-27 20:14:03 -0500

    Changed file structure

--- views/index.php
+++ views/index.php
@@ -5,7 +5,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-<style>
+<link rel="stylesheet" href="../static/styles.css">
+<!-- <style>
 body,h1 {font-family: "Raleway", sans-serif}
 body, html {height: 100%}
 .bgimg {
@@ -14,7 +15,7 @@ body, html {height: 100%}
     background-position: center;
     background-size: cover;
 }
-</style>
+</style> -->
 <body>
 
 <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
